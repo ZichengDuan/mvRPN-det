14da867ad52f3e86019534769d218845b631cb84
ZichengDuan <846278890@qq.com>
2021-08-29 20:54:57 +0800

    完成opensource RM

---
+++ opensource/rename_txt.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+target_path = "/home/dzc/Data/opensource/annotations/"
+root_path = "/home/dzc/Data/opensource/light/annotations/"
+
+target_file_num = len(os.listdir(target_path))
+root_file_num = len(os.listdir(root_path))
+
+print(target_file_num)
+
+i = 0
+name = 0
+while i < root_file_num:
+    idx = str(name)
+    # if 6 - len(idx) > 0:
+    #     for j in range(6 - len(idx)):
+    #         idx = "0" + idx
+
+    filename = os.path.join(root_path, "%s.json" % idx)
+
+    if os.path.exists(filename):
+        shutil.copy(filename, os.path.join(target_path, "%d.json" % (i + target_file_num)))
+        i += 1
+        name += 1
+    else:
+        name += 1
