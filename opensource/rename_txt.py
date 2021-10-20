7ebabc618263c79fac7b1bafbe9d6b8da7d21d31
ZichengDuan <846278890@qq.com>
2021-10-20 11:28:56 +0800

    dzc

--- opensource/rename_txt.py
+++ opensource/rename_txt.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-target_path = "/home/dzc/Data/opensource/annotations/"
-root_path = "/home/dzc/Data/opensource/raws/light/annotations/"
+target_path = "/home/dzc/Data/zed/new_img/"
+root_path = "/home/dzc/Data/zed/1_img/"
 
 target_file_num = len(os.listdir(target_path))
 root_file_num = len(os.listdir(root_path))
@@ -17,10 +17,10 @@ while i < root_file_num:
     #     for j in range(6 - len(idx)):
     #         idx = "0" + idx
 
-    filename = os.path.join(root_path, "%s.json" % idx)
+    filename = os.path.join(root_path, "%s.jpg" % idx)
 
     if os.path.exists(filename):
-        shutil.copy(filename, os.path.join(target_path, "%d.json" % (i + target_file_num)))
+        shutil.copy(filename, os.path.join(target_path, "%d.jpg" % (i + target_file_num)))
         i += 1
         name += 1
     else:
--- opensource/zed_obj_data.py
+++ opensource/zed_obj_data.py
@@ -8,20 +8,19 @@ import os
 
 def read_txt(left_right_dir):
     l_lhs = None
-    for m in range(0, 1600):
+    for m in range(0, 2806):
         print(m)
         idx = str(m)
         # if 6 - len(idx) > 0:
         #     for j in range(6 - len(idx)):
         #         idx = "0" + idx
 
-        if not os.path.exists(left_right_dir + "/left1/%s.txt" % idx):
+        if not os.path.exists(left_right_dir + "/%s.txt" % idx):
             continue
 
-        left = open(left_right_dir + "/left1/%s.txt" % idx)
-        right = open(left_right_dir + "/right2/%s.txt" % idx)
+        left = open(left_right_dir + "/%s.txt" % idx)
         datas = []
-        annotation = open("/home/dzc/Data/4carreal_dia0526/annotations/%d.json" % (m), 'w')
+        annotation = open("/home/dzc/Data/zed/annotations/%d.json" % (m), 'w')
 
         od_xmax = []
         od_xmin = []
@@ -29,11 +28,9 @@ def read_txt(left_right_dir):
         od_ymin = []
         cordss = []
         left_lines = left.readlines()
-        right_lines = right.readlines()
 
         for i in range(len(left_lines)):
             l_lhs, l_rhs = left_lines[i].split(":")
-            r_lhs, r_rhs = right_lines[i].split(":")
 
             if l_lhs == "blue1":
                 l_lhs = 0
@@ -45,7 +42,6 @@ def read_txt(left_right_dir):
                 l_lhs = 3
 
             cont_left = l_rhs.split( )
-            cont_right = r_rhs.split( )
             # if i < 3:
             #     cont_left[-1] = cont_left[-1][:-2] # 去除换行符
 
@@ -54,19 +50,16 @@ def read_txt(left_right_dir):
             #     print(p)
             if len(left_lines) == 3:
                 world_x, world_y, left_xmax, left_ymax, left_xmin, left_ymin, _ = [float(tmp) for tmp in cont_left]
-                right_xmax, right_ymax, right_xmin, right_ymin = [float(tmp) for tmp in cont_right[2:-1]]
                 if i == 1:
                     world_x *= 1000
                     world_y *= 1000
             elif len(left_lines) == 4:
                 world_x, world_y, left_xmax, left_ymax, left_xmin, left_ymin, _, angle = [float(tmp) for tmp in cont_left]
-                right_xmax, right_ymax, right_xmin, right_ymin = [float(tmp) for tmp in cont_right[2:-2]]
                 if i == 2:
                     world_x *= 1000
                     world_y *= 1000
             elif len(left_lines) == 2:
                 world_x, world_y, left_xmax, left_ymax, left_xmin, left_ymin, angle = [float(tmp) for tmp in cont_left]
-                right_xmax, right_ymax, right_xmin, right_ymin = [float(tmp) for tmp in cont_right[2:-1]]
             pID = i
             # ## 将角度转换为0-360度
             angle = float(cont_left[-1]) # 1carreal data此处是-2
@@ -88,23 +81,19 @@ def read_txt(left_right_dir):
             world_y = Const.grid_height * 10 - world_y
 
             lold = np.array([left_xmax, left_xmin, left_ymax, left_ymin])
-            rold = np.array([right_xmax, right_xmin, right_ymax, right_ymin])
             # print("old", lold)
             # 裁剪，x控制到0-640,y控制到0-480
-            lnew = np.clip(lold, [0,0,0,0], [640, 640, 480, 480])
-            rnew = np.clip(rold, [0,0,0,0], [640, 640, 480, 480])
+            lnew = np.clip(lold, [0,0,0,0], [1280, 1280, 720, 720])
 
             # 裁剪前后框的面积
             loarea = (left_xmax - left_xmin) * (left_xmax - left_xmin)
-            roarea = (right_xmax - right_xmin) * (right_ymax - right_ymin)
 
             lnarea = (lnew[0] - lnew[1]) * (lnew[2] - lnew[3])
-            rnarea = (rnew[0] - rnew[1]) * (rnew[2] - rnew[3])
             # print("new", lnew)
             # 全出界，坐标全出，或者是裁剪后边框所占面积小于原来的0.3
             # print(lnarea, loarea, lnarea / loarea < 0.3)
             # print(lnew.all())
-            if np.sum(lnew) == 0 or np.sum(lnew) == 480 * 2 + 640 * 2 or lnarea / loarea < 0.3:
+            if np.sum(lnew) == 0 or np.sum(lnew) == 720 * 2 + 1280 * 2 or lnarea / loarea < 0.3:
                 lnew = (np.zeros(4) - 1).astype(np.int32)
 
             # if m == 25 and i == 0:
@@ -113,13 +102,9 @@ def read_txt(left_right_dir):
             #     # print(right_xmax, right_xmin, right_ymax, right_ymin)
             #     print(rnarea / roarea)
 
-            if np.sum(rnew) == 0 or np.sum(rnew) == 480 * 2 + 640 * 2 or rnarea / roarea < 0.3:
-                rnew = (np.zeros(4) - 1).astype(np.int32)
 
             left_xmax, left_xmin, left_ymax, left_ymin = lnew
             left_xmax, left_xmin, left_ymax, left_ymin = int(left_xmax), int(left_xmin), int(left_ymax), int(left_ymin)
-            right_xmax, right_xmin, right_ymax, right_ymin = rnew
-            right_xmax, right_xmin, right_ymax, right_ymin = int(right_xmax), int(right_xmin), int(right_ymax), int(right_ymin)
             # if m == 25 and i == 0:
             #
             #     print(rnew)
@@ -182,7 +167,7 @@ def read_txt(left_right_dir):
             data["wx"] = world_x
             data["wy"] = world_y
             view0 = {"viewNum": 0, "xmax": left_xmax, "xmin": left_xmin, "ymax": left_ymax, "ymin": left_ymin}
-            view1 = {"viewNum": 1, "xmax": right_xmax, "xmin": right_xmin, "ymax": right_ymax, "ymin": right_ymin}
+            view1 = {"viewNum": 1, "xmax": 0, "xmin": 0, "ymax": 0, "ymin": 0}
             data["views"] = [view0, view1]
             data["xmin_od"] = xmin_od
             data["xmax_od"] = xmax_od
