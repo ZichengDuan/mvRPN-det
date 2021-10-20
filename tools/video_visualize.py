a0f575971962cd16c27f109cf5a03e0d8e65eb6e
ZichengDuan <846278890@qq.com>
2021-10-20 18:06:17 +0800

    搞了几个视频

--- tools/video_visualize.py
+++ tools/video_visualize.py
@@ -1,13 +1,14 @@
 from EX_CONST import Const
 import cv2
-fps = 12 #视频每秒1帧
+fps = 10 #视频每秒1帧
 size = (640, 480)
-video = cv2.VideoWriter("/home/dzc/Videos/pred_location.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size)   #视频保存在当前目录下, 格式为 motion-jpeg codec，图片颜色失真比较小
+size = (1280, 720)
+video = cv2.VideoWriter("/home/dzc/Videos/ori_MVM3D.avi", cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, size)   #视频保存在当前目录下, 格式为 motion-jpeg codec，图片颜色失真比较小
 
-for i in list(range(1800, 2100)) + list(range(3500, 3600)):
+for i in list(range(0, 2805)):
     print(i)
     # img = cv2.imread("%s/%d.jpg" % (Const.imgsavedir, i))
-    img = cv2.imread("/home/dzc/Desktop/CASIA/proj/mvRPN-det/results/images/3d_box_blend/%d.jpg" % (i))
+    img = cv2.imread("/home/dzc/Data/zed/new_img/%d.jpg" % (i))
     video.write(img)
 
 video.release()
