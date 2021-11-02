2ab466b4d0ba5f5665d110773eca8d045292c142
ZichengDuan <846278890@qq.com>
2021-11-02 09:35:02 +0800

    dzc

--- 4carreal_object_detection/rename_txt.py
+++ 4carreal_object_detection/rename_txt.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 
-target_path = "/home/dzc/Data/mix/bevimgs"
-root_path = "/home/dzc/Data/4carreal_dia0526/bevimgs"
+target_path = "/home/dzc/Data/opensource/seperate_anno"
+root_path = "/home/dzc/Data/opensource/annotations"
 
 target_file_num = len(os.listdir(target_path))
 root_file_num = len(os.listdir(root_path))
@@ -13,14 +13,14 @@ i = 0
 name = 0
 while i < root_file_num:
     idx = str(name)
-    if 6 - len(idx) > 0:
-        for j in range(6 - len(idx)):
-            idx = "0" + idx
+    # if 6 - len(idx) > 0:
+    #     for j in range(6 - len(idx)):
+    #         idx = "0" + idx
 
-    filename = os.path.join(root_path, "%s.jpg" % idx)
+    filename = os.path.join(root_path, "%s.json" % idx)
 
     if os.path.exists(filename):
-        shutil.copy(filename, os.path.join(target_path, "%d.jpg" % (i + target_file_num)))
+        shutil.copy(filename, os.path.join(target_path, "%d.json" % (i + target_file_num)))
         i += 1
         name += 1
     else:
